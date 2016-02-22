682b7ff73c424860447e910420c54136d083fa46
Raunak Lohiya <rklohiya1996@gmail.com>
2016-02-22 19:35:50 +0530

    correct functioning JSON response

--- app/src/main/java/com/example/raunak/moodleplus/MainActivity.java
+++ app/src/main/java/com/example/raunak/moodleplus/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.raunak.moodleplus;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,8 +19,15 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.StringReader;
 
@@ -26,6 +35,27 @@ public class MainActivity extends AppCompatActivity {
     private String username,password;
     private EditText editTextUsername, editTextPassword;
     private Button loginButton;
+    private TextView mTextView;
+    private TextView usernameTextView ;
+    private TextView passwordTextView;
+    // json object response url
+    //private String urlJsonObj = "http://api.androidhive.info/volley/person_object.json";
+
+    // json array response url
+    //private String urlJsonArry = "http://api.androidhive.info/volley/person_array.json";
+    private String urlJsonObj = "http://10.192.13.234:8000/default/login.json?userid=cs5110300&password=shubham";
+    private String urlJsonArry = "http://10.192.13.234:8000/default/login.json?userid=cs5110300&password=shubham";
+
+    private static String TAG = MainActivity.class.getSimpleName();
+    private Button btnMakeObjectRequest, btnMakeArrayRequest;
+
+    // Progress dialog
+    private ProgressDialog pDialog;
+
+    private TextView txtResponse;
+
+    // temporary string to show the parsed response
+    private String jsonResponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,10 +73,38 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        editTextUsername = (EditText)findViewById(R.id.TFusername);
+        btnMakeObjectRequest = (Button) findViewById(R.id.btnObjRequest);
+        btnMakeArrayRequest = (Button) findViewById(R.id.btnArrayRequest);
+        txtResponse = (TextView) findViewById(R.id.txtResponse);
+
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Please wait...");
+        pDialog.setCancelable(false);
+
+        btnMakeObjectRequest.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // making json object request
+                makeJsonObjectRequest();
+            }
+        });
+
+        btnMakeArrayRequest.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // making json array request
+                makeJsonArrayRequest();
+            }
+        });
+        /*editTextUsername = (EditText)findViewById(R.id.TFusername);
         editTextPassword = (EditText)findViewById(R.id.TFpassword);
         username = editTextUsername.getText().toString();
         password = editTextPassword.getText().toString();
+        usernameTextView.setText(username);
+        passwordTextView.setText(password);
+
         final String REGISTER_URL = "http://192.168.43.151:8000/default/login.json?userid="+username+"&password="+password;
         loginButton = (Button)findViewById(R.id.loginButton);
 
@@ -54,13 +112,136 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v){
                 getStringRequest(REGISTER_URL);
             }
-        });
+        });*/
+
+
     }
 
-    private void getStringRequest(String register_url) {
-        final TextView mTextView = (TextView)findViewById(R.id.editTextresponse);
-        final TextView usernameTextView = (TextView)findViewById(R.id.editTextusername);
-        final TextView passwordTextView = (TextView)findViewById(R.id.editTextpassword);
+    private void makeJsonObjectRequest() {
+        showpDialog();
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                urlJsonObj, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d(TAG, response.toString());
+
+                try {
+                    JSONObject user = response.getJSONObject("user");
+                    String last_name = user.getString("last_name");
+                    //String lname = last_name.getString("last_name");
+                    // Parsing json object response
+                    // response will be a json object
+                    /*String name = response.getString("last_name");
+                    String email = response.getString("email");
+                    JSONObject phone = response.getJSONObject("phone");
+                    String home = phone.getString("home");
+                    String mobile = phone.getString("mobile");*/
+
+                    jsonResponse = "";
+                    jsonResponse += "Name: " + last_name + "\n\n";
+                    /*jsonResponse += "Email: " + email + "\n\n";
+                    jsonResponse += "Home: " + home + "\n\n";
+                    jsonResponse += "Mobile: " + mobile + "\n\n";*/
+
+                    txtResponse.setText(jsonResponse);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(),
+                            "Error: " + e.getMessage(),
+                            Toast.LENGTH_LONG).show();
+                    txtResponse.setText(e.toString());
+                }
+
+                hidepDialog();
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+                // hide the progress dialog
+                hidepDialog();
+            }
+        });
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(jsonObjReq);
+    }
+
+    private void makeJsonArrayRequest() {
+        showpDialog();
+
+        JsonArrayRequest req = new JsonArrayRequest(urlJsonArry,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, response.toString());
+
+                        try {
+                            // Parsing json array response
+                            // loop through each json object
+                            jsonResponse = "";
+                            for (int i = 0; i < response.length(); i++) {
+
+                                JSONObject person = (JSONObject) response
+                                        .get(i);
+
+                                String name = person.getString("name");
+                                String email = person.getString("email");
+                                JSONObject phone = person
+                                        .getJSONObject("phone");
+                                String home = phone.getString("home");
+                                String mobile = phone.getString("mobile");
+
+                                jsonResponse += "Name: " + name + "\n\n";
+                                jsonResponse += "Email: " + email + "\n\n";
+                                jsonResponse += "Home: " + home + "\n\n";
+                                jsonResponse += "Mobile: " + mobile + "\n\n\n";
+
+                            }
+
+                            txtResponse.setText(jsonResponse);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(getApplicationContext(),
+                                    "Error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                            txtResponse.setText(e.toString());
+                        }
+
+                        hidepDialog();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_SHORT).show();
+                hidepDialog();
+            }
+        });
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(req);
+
+    }
+
+    private void showpDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hidepDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
+    }
+    /*private void getStringRequest(String register_url) {
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
 
@@ -86,7 +267,7 @@ public class MainActivity extends AppCompatActivity {
         });
         // Add the request to the RequestQueue.
         queue.add(stringRequest);
-    }
+    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/com/example/raunak/moodleplus/homepage.java
@@ -0,0 +1,7 @@
+package com.example.raunak.moodleplus;
+
+/**
+ * Created by raunak$ on 22-02-2016.
+ */
+public class homepage {
+}
