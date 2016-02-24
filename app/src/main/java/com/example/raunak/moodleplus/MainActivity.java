97cc61a33b277cab5ac392d52ec25446c0742d4f
rajapur <rklohiya1996@gmail.com>
2016-02-24 15:14:31 +0530

    Main Activity with cookies and JSON response

--- app/src/main/java/com/example/raunak/moodleplus/MainActivity.java
+++ app/src/main/java/com/example/raunak/moodleplus/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.raunak.moodleplus;
 
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,6 +12,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.CookieSyncManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -20,31 +23,46 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpClientStack;
+import com.android.volley.toolbox.HttpStack;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.StringReader;
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.net.CookiePolicy;
+import java.net.CookieStore;
+import java.util.HashMap;
+import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+    private static final String SET_COOKIE_KEY = "Set-Cookie";
+    private static final String COOKIE_KEY = "Cookie";
+    private static final String SESSION_COOKIE = "sessionid";
+
+    /*private static MainActivity _instance;
+    private RequestQueue _requestQueue;
+    private SharedPreferences _preferences;*/
+
+    public static final String USER_NAME = "username";
+    public static final String PASS_WORD = "password";
+    private static final String DOMAIN = "http://10.192.33.139:8000";
     private String username,password;
     private EditText editTextUsername, editTextPassword;
     private Button loginButton;
     private TextView mTextView;
     private TextView usernameTextView ;
     private TextView passwordTextView;
-    // json object response url
-    //private String urlJsonObj = "http://api.androidhive.info/volley/person_object.json";
 
+    // json object response url
+    private String urlJsonObj;
     // json array response url
-    //private String urlJsonArry = "http://api.androidhive.info/volley/person_array.json";
-    //private String urlJsonObj = "http://10.192.13.234:8000/default/login.json?userid=cs5110300&password=shubham";
-    //private String urlJsonArry = "http://10.192.13.234:8000/default/login.json?userid=cs5110300&password=shubham";
+    private String urlJsonArry;
 
     private static String TAG = MainActivity.class.getSimpleName();
     private Button btnMakeObjectRequest, btnMakeArrayRequest;
@@ -59,10 +77,18 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Context mContext = this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        editTextUsername = (EditText)findViewById(R.id.TFusername);
+        editTextPassword = (EditText)findViewById(R.id.TFpassword);
+        username = editTextUsername.getText().toString();
+        password = editTextPassword.getText().toString();
+        usernameTextView.setText(username);
+        passwordTextView.setText(password);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -72,6 +98,13 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+        // json object response url
+        urlJsonObj = DOMAIN+"/default/login.json?userid="+username+"&password="+password;
+
+        // json array response url
+        urlJsonArry = DOMAIN+"/default/login.json?userid="+username+"&password="+password;
 
         btnMakeObjectRequest = (Button) findViewById(R.id.btnObjRequest);
         btnMakeArrayRequest = (Button) findViewById(R.id.btnArrayRequest);
@@ -98,23 +131,22 @@ public class MainActivity extends AppCompatActivity {
                 makeJsonArrayRequest();
             }
         });
-        editTextUsername = (EditText)findViewById(R.id.TFusername);
-        editTextPassword = (EditText)findViewById(R.id.TFpassword);
-        username = editTextUsername.getText().toString();
-        password = editTextPassword.getText().toString();
-        usernameTextView.setText(username);
-        passwordTextView.setText(password);
 
-        final String REGISTER_URL = "http://192.168.43.151:8000/default/login.json?userid="+username+"&password="+password;
+
+        /*final String REGISTER_URL = "http://192.168.43.151:8000/default/login.json?userid=vinay&password=vinay";
         loginButton = (Button)findViewById(R.id.loginButton);
 
         loginButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 getStringRequest(REGISTER_URL);
             }
-        });
+        });*/
+        CookieManager manager = new CookieManager(null,CookiePolicy.ACCEPT_ALL);
+        CookieHandler.setDefault(manager);
 
-
+        /*_instance = this;
+        _preferences= PreferenceManager.getDefaultSharedPreferences(this);
+        _requestQueue = Volley.newRequestQueue(this);*/
     }
 
     private void makeJsonObjectRequest() {
@@ -128,8 +160,9 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, response.toString());
 
                 try {
+
                     JSONObject user = response.getJSONObject("user");
-                    String last_name = user.getString("last_name");
+                    String user_name = user.getString("username");
                     //String lname = last_name.getString("last_name");
                     // Parsing json object response
                     // response will be a json object
@@ -140,19 +173,22 @@ public class MainActivity extends AppCompatActivity {
                     String mobile = phone.getString("mobile");*/
 
                     jsonResponse = "";
-                    jsonResponse += "Name: " + last_name + "\n\n";
+                    jsonResponse += "Welcome "+user_name + "\n\n";
                     /*jsonResponse += "Email: " + email + "\n\n";
                     jsonResponse += "Home: " + home + "\n\n";
                     jsonResponse += "Mobile: " + mobile + "\n\n";*/
 
-                    txtResponse.setText(jsonResponse);
+                    //txtResponse.setText(jsonResponse);
+
+                    Intent intent = new Intent(MainActivity.this, MainCourse.class);
+                    startActivity(intent);
 
                 } catch (JSONException e) {
                     e.printStackTrace();
                     Toast.makeText(getApplicationContext(),
                             "Error: " + e.getMessage(),
                             Toast.LENGTH_LONG).show();
-                    txtResponse.setText(e.toString());
+                    //txtResponse.setText(e.toString());
                 }
 
                 hidepDialog();
@@ -169,11 +205,71 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
         // Adding request to request queue
         AppController.getInstance().addToRequestQueue(jsonObjReq);
     }
 
-    
+    private void makeJsonArrayRequest() {
+        showpDialog();
+
+        JsonArrayRequest req = new JsonArrayRequest(urlJsonArry,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, response.toString());
+
+                        try {
+                            // Parsing json array response
+                            // loop through each json object
+                            jsonResponse = "";
+                            for (int i = 0; i < response.length(); i++) {
+
+                                JSONObject person = (JSONObject) response
+                                        .get(i);
+
+                                String name = person.getString("name");
+                                String email = person.getString("email");
+                                JSONObject phone = person
+                                        .getJSONObject("phone");
+                                String home = phone.getString("home");
+                                String mobile = phone.getString("mobile");
+
+                                jsonResponse += "Name: " + name + "\n\n";
+                                jsonResponse += "Email: " + email + "\n\n";
+                                jsonResponse += "Home: " + home + "\n\n";
+                                jsonResponse += "Mobile: " + mobile + "\n\n\n";
+
+                            }
+
+                            txtResponse.setText(jsonResponse);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(getApplicationContext(),
+                                    "Error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                            txtResponse.setText(e.toString());
+                        }
+
+                        hidepDialog();
+                        Intent intent = new Intent(MainActivity.this, MainView.class);
+                        startActivity(intent);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_SHORT).show();
+                hidepDialog();
+            }
+        });
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(req);
+
+    }
 
     private void showpDialog() {
         if (!pDialog.isShowing())
@@ -187,9 +283,12 @@ public class MainActivity extends AppCompatActivity {
     /*private void getStringRequest(String register_url) {
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        Map<String,String> params = new HashMap<>();
+        params.put(USER_NAME,username);
+        params.put(PASS_WORD,password);
 
         // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET,register_url,
+        /*final StringRequest stringRequest = new StringRequest(Request.Method.GET,register_url, params,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
@@ -198,6 +297,7 @@ public class MainActivity extends AppCompatActivity {
                         mTextView.setText("Response is: " + response);
                         usernameTextView.setText(username);
                         passwordTextView.setText(password);
+                        Response<String> stringResponse = stringRequest.parseNetworkResponse(response);
                     }
                 }, new Response.ErrorListener() {
             @Override
@@ -208,9 +308,16 @@ public class MainActivity extends AppCompatActivity {
                 //Toast.makeText(MainActivity.this, error, Toast.LENGTH_LONG).show();
             }
         });
+
         // Add the request to the RequestQueue.
-        queue.add(stringRequest);
-    }*/
+        queue.add(stringRequest);*/
+        // If you need to directly manipulate cookies later on, hold onto this client
+// object as it gives you access to the Cookie Store
+
+// Optionally, you can just use the default CookieManager
+        /*CookieManager manager = new CookieManager();
+        CookieHandler.setDefault(manager);
+}*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -221,12 +328,138 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    /*protected Response parseNetworkResponse(NetworkResponse response) {
+        Map headers = response.headers;
+        String cookie = headers.get("Set-Cookie").toString();//toString()
+        Preferences.getInstance().saveCookie(cookie);
+        return super.parseNetworkResponse(response);
+    }
+
+    public void saveCookie(String cookie) {
+        if (cookie == null) {
+            //the server did not return a cookie so we wont have anything to save
+            return;
+        }
+        // Save in the preferences
+        SharedPreferences prefs = getPreferences();
+        if (null == prefs) {
+            return;
+        }
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("cookie", cookie);
+        editor.commit();
+    }
+
+    @Override
+    public Map getHeaders() throws AuthFailureError {
+        Map headers = new HashMap();
+        if(!Preferences.getInstance().getCookie().equals(""))
+            headers.put("Cookie", Preferences.getInstance().getCookie());
+        return headers;
+    }
+
+    public String getCookie()
+    {
+        SharedPreferences prefs = getPreferences();
+        String cookie = prefs.getString("cookie", "");
+        if (cookie.contains("expires")) {
+/** you might need to make sure that your cookie returns expires when its expired. I also noted that cokephp returns deleted */
+            /*removeCookie();
+            return "";
+        }
+        return cookie.toString();//toString()
+    }
+
+    public void removeCookie() {
+        SharedPreferences prefs = getPreferences();
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.remove("cookie");
+        editor.commit();
+    }
+
+    public static MainActivity get() {
+        return _instance;
+    }
+
+    /**
+     * Checks the response headers for session cookie and saves it
+     * if it finds it.
+     * @param headers Response Headers.
+     */
+    /*public final void checkSessionCookie(Map<String, String> headers) {
+        if (headers.containsKey(SET_COOKIE_KEY)
+                && headers.get(SET_COOKIE_KEY).startsWith(SESSION_COOKIE)) {
+            String cookie = headers.get(SET_COOKIE_KEY);
+            if (cookie.length() > 0) {
+                String[] splitCookie = cookie.split(";");
+                String[] splitSessionId = splitCookie[0].split("=");
+                cookie = splitSessionId[1];
+                SharedPreferences.Editor prefEditor = _preferences.edit();
+                prefEditor.putString(SESSION_COOKIE, cookie);
+                prefEditor.commit();
+            }
+        }
+    }
+
+    /**
+     * Adds session cookie to headers if exists.
+     * @param headers
+     */
+    /*public final void addSessionCookie(Map<String, String> headers) {
+        String sessionId = _preferences.getString(SESSION_COOKIE, "");
+        if (sessionId.length() > 0) {
+            StringBuilder builder = new StringBuilder();
+            builder.append(SESSION_COOKIE);
+            builder.append("=");
+            builder.append(sessionId);
+            if (headers.containsKey(COOKIE_KEY)) {
+                builder.append("; ");
+                builder.append(headers.get(COOKIE_KEY));
+            }
+            headers.put(COOKIE_KEY, builder.toString());
+        }
+    }*/
+
+    /*private static void clearCookiesForDomain(Context context, String domain) {
+        // This is to work around a bug where CookieManager may fail to instantiate if CookieSyncManager
+        // has never been created.
+        CookieSyncManager syncManager = CookieSyncManager.createInstance(context);
+        syncManager.sync();
+
+        CookieManager cookieManager = CookieManager.getInstance();
+
+        String cookies = cookieManager.getCookie(domain);
+        if (cookies == null) {
+            return;
+        }
+
+        String[] splitCookies = cookies.split(";");
+        for (String cookie : splitCookies) {
+            String[] cookieParts = cookie.split("=");
+            if (cookieParts.length > 0) {
+                String newCookie = cookieParts[0].trim();
+                cookieManager.setCookie(domain, newCookie);
+            }
+        }
+        //cookieManager.removeExpiredCookie();
+    }*/
+
+    private void startnewActivity(){
+        Intent intent = new Intent(MainActivity.this,MainView.class);
+        startActivity(intent);
+    }
+
 }
